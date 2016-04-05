fcfdcc07183ecd0b92361fed6aae6dd40c40395f
Conrad Steinmetz <conrad.steinmetz@gmx.de>
2016-04-05 12:45:22 +0200

    Flares added and Weapon attached to Player and Weapon Selection possible

--- CowboysVsZombies/Assets/Script/Player.cs
+++ CowboysVsZombies/Assets/Script/Player.cs
@@ -11,13 +11,68 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+	public GameObject Shotgun;
+	public GameObject Revolver;
+	public GameObject Rifle;
+	public GameObject WeaponSpawn;
+	private string weaponSelected;
+	public GameObject actualWeapon;
+
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		weaponSelected="Shotgun";
+		actualWeapon= (GameObject)Instantiate(getactualWeapon(), WeaponSpawn.transform.position, Quaternion.identity);
+		actualWeapon.transform.parent = transform;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.C)) 
+		{
+			Destroy (actualWeapon);
+			if (weaponSelected.Equals ("Shotgun")) {
+				changeWeapon ("Revolver");
+			}
+		 	else if (weaponSelected.Equals ("Revolver"))
+		    {
+				changeWeapon ("Rifle");
+				
+			} 
+			else 
+			{
+				changeWeapon ("Shotgun");
+			}
+		}
+	}
+
+	void changeWeapon(string newWeapon)
+	{
+		if (weaponSelected.Equals (newWeapon)) {
+			;
+		} 
+		else {
+			weaponSelected = newWeapon;
+			actualWeapon= (GameObject)Instantiate(getactualWeapon(), WeaponSpawn.transform.position, Quaternion.identity);
+			actualWeapon.transform.parent = transform;
+		}
+	}
+
+	GameObject getactualWeapon()
+	{
+		if (weaponSelected.Equals ("Shotgun")) 
+		{
+			return Shotgun;
+		}
+		else if (weaponSelected.Equals ("Rifle")) 
+		{
+			return Rifle;
+		}
+		else
+		{
+			return Revolver;
+		}
 	}
 }

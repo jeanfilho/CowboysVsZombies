443bbbb42d5495edf0558998ff69d85edba84a7a
Florian <f.buschek@gmail.com>
2016-04-07 13:06:21 +0200

    Added GUI Assets.

---
+++ CowboysVsZombies/Assets/Script/ActionSign.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionSign : MonoBehaviour {
+
+    public float timer = 1;
+    private float counter = 0;
+
+	// Update is called once per frame
+	void Update () {
+        counter += Time.deltaTime;
+        if (counter >= timer)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
---
+++ CowboysVsZombies/Assets/Script/PlayerHUD.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerHUD : MonoBehaviour {
+
+    public Image healthBarHUD;
+    public Text ammoLoaded;
+    public Text ammoCurrent;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+     //   updateHealth();
+     //   updateAmmo();
+	}
+
+    public void updateHealth()
+    {
+        healthBarHUD.GetComponent<Image>().fillAmount = (GameData.Instance.getPlayerHealth()) / 100.0f;
+    }
+
+    public void updateAmmo()
+    {
+    //    ammoLoaded.text = ;
+    //    ammoCurrent.text = ;
+    }
+}

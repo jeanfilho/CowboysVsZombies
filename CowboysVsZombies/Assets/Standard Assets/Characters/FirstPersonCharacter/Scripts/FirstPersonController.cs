c7d2c1a9c7818e5372b44df3a64e5a613a63fa33
Jean Paul <jeanfilho@gmail.com>
2016-04-12 13:59:09 +0200

    Removed jumping and running to avoid unintended behavior in the game

--- CowboysVsZombies/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ CowboysVsZombies/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -63,10 +63,12 @@ namespace UnityStandardAssets.Characters.FirstPerson
         {
             RotateView();
             // the jump state needs to read here to make sure it is not missed
+			/*
             if (!m_Jump)
             {
                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
             }
+            */
 
             if (!m_PreviouslyGrounded && m_CharacterController.isGrounded)
             {
@@ -212,7 +214,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
 #if !MOBILE_INPUT
             // On standalone builds, walk/run speed is modified by a key press.
             // keep track of whether or not the character is walking or running
-            m_IsWalking = !Input.GetKey(KeyCode.LeftShift);
+            //m_IsWalking = !Input.GetKey(KeyCode.LeftShift);
 #endif
             // set the desired speed to be walking or running
             speed = m_IsWalking ? m_WalkSpeed : m_RunSpeed;

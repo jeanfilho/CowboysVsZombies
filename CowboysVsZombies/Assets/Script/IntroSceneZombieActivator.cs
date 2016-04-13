c4e6f38ce0b13e9f06f0960d42655ed0487cf740
Florian <f.buschek@gmail.com>
2016-04-13 19:13:58 +0200

    Added a scene for trailer filming.

---
+++ CowboysVsZombies/Assets/Script/IntroSceneZombieActivator.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroSceneZombieActivator : MonoBehaviour {
+
+    // This script is used to activate the zombies in the intro scene.
+
+    public GameObject[] zombieList = new GameObject[0];
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        for (int i = 0; i < zombieList.Length; i++)
+        {
+            zombieList[i].SetActive(true);
+        }
+    }
+}
